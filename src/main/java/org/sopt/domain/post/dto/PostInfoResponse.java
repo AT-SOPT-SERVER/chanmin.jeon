d8e2d0fdb0a10612ef55c8dd4d2918b52e9799b8
Jeon Chan Min <chanmin010702@gmail.com>
2025-05-03 02:11:21 +0900

    refactor: PostInfoRespons로 클래스 이름 변경

--- src/main/java/org/sopt/domain/post/dto/PostInfoResponse.java
+++ src/main/java/org/sopt/domain/post/dto/PostInfoResponse.java
@@ -2,15 +2,15 @@ package org.sopt.domain.post.dto;
 
 import org.sopt.domain.post.entity.Post;
 
-public record PostSummaryResponse(
+public record PostInfoResponse(
     Long postId,
     String title,
     String author,
     String tag
 ) {
 
-  public static PostSummaryResponse from(Post post) {
-    return new PostSummaryResponse(
+  public static PostInfoResponse from(Post post) {
+    return new PostInfoResponse(
         post.getId(),
         post.getTitle(),
         post.getUser().getAuthor(),
