8c548d5345672a66a1fff6b2177c9f0cded3e811
Jeon Chan Min <chanmin010702@gmail.com>
2025-05-02 22:25:43 +0900

    feat: UserService 구현 - 사용자 유효성 검증 및 조회 비즈니스 로직 분리

---
+++ src/main/java/org/sopt/domain/user/service/UserService.java
@@ -0,0 +1,44 @@
+package org.sopt.domain.user.service;
+
+import org.sopt.domain.user.exception.UserErrorCode;
+import org.sopt.exception.CustomException;
+import org.sopt.domain.user.dto.UserCreateRequest;
+import org.sopt.domain.user.entity.User;
+import org.sopt.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+
+  @Transactional
+  public void createUser(UserCreateRequest request) {
+    validateNickName(request.author());
+
+    if (userRepository.existsByNickname(request.author())) {
+      throw new CustomException(UserErrorCode.DUPLICATE_NICKNAME);
+    }
+
+    userRepository.save(new User(request.author()));
+  }
+
+  private void validateNickName(String nickname) {
+    if (nickname == null || nickname.isBlank()) {
+      throw new CustomException(UserErrorCode.NICKNAME_REQUIRED);
+    }
+
+    if (nickname.length() > 10) {
+      throw new CustomException(UserErrorCode.NICKNAME_TOO_LONG);
+    }
+
+
+  }
+
+}
