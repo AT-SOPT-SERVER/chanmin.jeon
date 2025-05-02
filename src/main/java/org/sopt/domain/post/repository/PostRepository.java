74b38229e8c91d4be797fe096b11becdb1b7d47f
Jeon Chan Min <chanmin010702@gmail.com>
2025-05-03 02:11:42 +0900

    refactor: jpa 메서드에서 @Query 방식으로 변경

--- src/main/java/org/sopt/domain/post/repository/PostRepository.java
+++ src/main/java/org/sopt/domain/post/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package org.sopt.domain.post.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.sopt.domain.post.entity.Post;
 import org.sopt.domain.post.type.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,21 +15,19 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
   boolean existsByTitle(String title);
 
-  List<Post> findByTitleContainingAndUser_AuthorContainingAndTag(String title, String author,
-      Tag tag);
-  List<Post> findByTitleContainingAndUser_AuthorContaining(String title, String author);
+  Optional<Post> findTopByOrderByCreatedAtDesc();
 
-  List<Post> findByTitleContainingAndTag(String title, Tag tag);
-  List<Post> findByUser_AuthorContainingAndTag(String author, Tag tag);
-
-  List<Post> findByTitleContaining(String title);
-
-  List<Post> findByUser_AuthorContaining(String author);
-
-  List<Post> findByTag(Tag tag);
-
-  Post findTopByOrderByCreatedAtDesc();
-
-  List<Post> searchByCondition(String title, String author, Tag tag);
-
+  @Query("""
+        SELECT p FROM Post p
+        JOIN p.user u
+        WHERE (:title IS NULL OR LOWER(p.title) LIKE LOWER(CONCAT('%', :title, '%')))
+        AND (:author IS NULL OR LOWER(u.author) LIKE LOWER(CONCAT('%', :author, '%')))
+        AND (:tag IS NULL OR p.tag = :tag)
+        ORDER BY p.createdAt DESC
+    """)
+  List<Post> searchByCondition(
+      @Param("title") String title,
+      @Param("author") String author,
+      @Param("tag") Tag tag
+  );
 }
--- src/main/java/org/sopt/domain/post/repository/PostRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.domain.post.repository;
-
-import java.util.List;
-import org.sopt.domain.post.entity.Post;
-import org.sopt.domain.post.type.Tag;
-
-public interface PostRepositoryCustom {
-  List<Post> searchByCondition(String title, String author, Tag tag);
-}
